ca06750a1a54a00f7ba36e06eb1eb33f9501dfcb
Edward Ren <eddie.ren.2013@gmail.com>
2017-07-24 18:13:24 -0400

    side tabs are now fully interactive

--- sideBar.js
+++ sideBar.js
@@ -1,7 +1,45 @@
 function profileClick() {
+  resetColors();
   document.getElementById("imagePlaceholderProfile").style.opacity = 0.9;
-  document.getElementById("myProfile").style.backgroundColor = "#CCCCCC"; 
-
+  document.getElementById("myProfile").style.backgroundColor = "#CCCCCC";
 
   console.log("profile clicked");
+}
+
+function experienceClick() {
+  resetColors();
+  document.getElementById("imagePlaceholderExperience").style.opacity = 0.9;
+  document.getElementById("myExperience").style.backgroundColor = "#CCCCCC";
+
+}
+
+function projectsClick() {
+  resetColors();
+  document.getElementById("imagePlaceholderProjects").style.opacity = 0.9;
+  document.getElementById("myProjects").style.backgroundColor = "#CCCCCC";
+}
+
+function networkClick() {
+  resetColors();
+  document.getElementById("imagePlaceholderNetwork").style.opacity = 0.9;
+  document.getElementById("myNetwork").style.backgroundColor = "#CCCCCC";
+}
+
+function languagesClick() {
+  resetColors();
+  document.getElementById("imagePlaceholderLanguages").style.opacity = 0.9;
+  document.getElementById("myLanguages").style.backgroundColor = "#CCCCCC";
+}
+
+function educationClick() {
+  resetColors();
+  document.getElementById("imagePlaceholderEducation").style.opacity = 0.9;
+  document.getElementById("myEducation").style.backgroundColor = "#CCCCCC";
+}
+
+function otherClick() {
+  resetColors();
+  document.getElementById("imagePlaceholderOther").style.opacity = 0.9;
+  document.getElementById("myOther").style.backgroundColor = "#CCCCCC";
+
 }
