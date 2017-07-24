fceaae3aac31df29bc73bd853e4393a29c346530
Edward Ren <eddie.ren.2013@gmail.com>
2017-07-24 14:42:46 -0400

    Added reset function on tab click

---
+++ resetBarColor.js
@@ -0,0 +1,25 @@
+function resetColors() {
+  document.getElementById("myProfile").style.backgroundColor = "#eef2f2";
+  document.getElementById("imagePlaceholderProfile").style.opacity = 0.6;
+
+  document.getElementById("myExperience").style.backgroundColor = "#eef2f2";
+  document.getElementById("imagePlaceholderExperience").style.opacity = 0.6;
+
+  document.getElementById("myProjects").style.backgroundColor = "#eef2f2";
+  document.getElementById("imagePlaceholderProjects").style.opacity = 0.6;
+
+  document.getElementById("myNetwork").style.backgroundColor = "#eef2f2";
+  document.getElementById("imagePlaceholderNetwork").style.opacity = 0.6;
+
+  document.getElementById("myLanguages").style.backgroundColor = "#eef2f2";
+  document.getElementById("imagePlaceholderLanguages").style.opacity = 0.6;
+
+  document.getElementById("myEducation").style.backgroundColor = "#eef2f2";
+  document.getElementById("imagePlaceholderEducation").style.opacity = 0.6;
+
+  document.getElementById("myOther").style.backgroundColor = "#eef2f2";
+  document.getElementById("imagePlaceholderOther").style.opacity = 0.6;
+
+
+
+}
