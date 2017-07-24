adac32c1f5715614cf3fc96f604706addb612625
Edward Ren <eddie.ren.2013@gmail.com>
2017-07-23 22:43:25 -0400

    finished template file and classes

---
+++ sideBar.js
@@ -0,0 +1,7 @@
+function profileClick() {
+  document.getElementById("imagePlaceholderProfile").style.opacity = 0.9;
+  document.getElementById("myProfile").style.backgroundColor = "#CCCCCC"; 
+
+
+  console.log("profile clicked");
+}
