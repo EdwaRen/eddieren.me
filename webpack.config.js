879f7c5d583411b936275dc5301e20d7511dfb0c
Edward Ren <eddie.ren.2013@gmail.com>
2017-08-17 23:24:20 +0800

    Deleted unnecessary images and added webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+process.env.NODE_ENV = 'production'
+
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './Components/Background.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'public')
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/}
+    ]
+  }
+}
