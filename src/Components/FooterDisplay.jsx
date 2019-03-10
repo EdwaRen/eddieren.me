c9ea8ae866b0cf9ee0a6003a9d3e6daf5c20eb71
Eddie <eddie.ren.2013@gmail.com>
2019-03-10 15:46:54 -0600

    Converted background photo from svg to png

--- src/Components/FooterDisplay.jsx
+++ src/Components/FooterDisplay.jsx
@@ -10,7 +10,7 @@ class FooterDisplay extends Component {
       // <Draggable>
           <div id="footer">
             <div id="bottomText">
-              <p>Source code is available <a target="_blank" href="https://github.com/EdwaRen/edwardren.me">here</a> <br />☕ Edward Ren 2017</p>
+              <p>Star this project on <a target="_blank" href="https://github.com/EdwaRen/edwardren.me">GitHub</a>!   <br />Edward Ren 2019 ☕</p>
             </div>
           </div>
         // {/* </Draggable> */}
