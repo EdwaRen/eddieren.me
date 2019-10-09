fa59df3b6e7c5d13eadedf489a2554aee7de270a
Eddie Ren <eddie.ren.2013@gmail.com>
2019-10-09 19:28:51 -0400

    Updated resume for TextNow

--- src/Info/PersonalInfo.js
+++ src/Info/PersonalInfo.js
@@ -26,11 +26,14 @@ var Info = [
   ],
   //Experience
   [
-    ["IBM", "", "Cognitive Software Engineer - Advanced Analytics", "Worked closely with data science team to deploy remote Jupyter notebook kernels and optimized dataframe operations with parallelism.<br /><br />Skills: Pandas, IPython, SQL, Keras", "Jan 2019 - April 2019", "Calgary", ""],
 
-    ["Blackberry - QNX", "", "Software Engineering Student - Open Source", "Worked with an amazing team to develop a big data licensing platform using the Django framework.<br /><br />Skills: Django, Docker, MongoDB, Tensorflow, Kafka", "May 2018 - August 2018", "Ottawa", ""],
+    ["TextNow", "", "Full Stack Developer", "Led and contributed to the development of internal tools with Ruby on Rails to deliver analytics for legal and mobile teams.<br /><br />Skills: Ruby on Rails, JavaScript, React, SQL", "Sept 2019 - Dec 2019", "Waterloo", ""],
 
-    ["WATonomous", "", "Full Stack Development Lead: <a href = 'www.watonomous.ca' target='_blank'> Website</a>", "Led versatile team to develop watonomous.ca with React and Node.js and deployed with Nginx.<br /><br />Skills: Node.js, Express.js, MongoDB, Nginx, React", "September 2017 - Present", "Waterloo", ""],
+    ["IBM", "", "Cognitive Software Engineer - Advanced Analytics", "Worked closely with data science team to deploy remote Jupyter notebook kernels and optimized dataframe operations with parallelism.<br /><br />Skills: Pandas, IPython, SQL, Keras", "Jan 2019 - Apr 2019", "Calgary", ""],
+
+    ["Blackberry - QNX", "", "Software Engineering Student - Open Source", "Worked with an amazing team to develop a big data licensing platform using the Django framework.<br /><br />Skills: Django, Docker, MongoDB, Tensorflow, Kafka", "May 2018 - Aug 2018", "Ottawa", ""],
+
+    ["WATonomous", "", "Full Stack Development Lead: <a href = 'www.watonomous.ca' target='_blank'> Website</a>", "Led versatile team to develop watonomous.ca with React and Node.js and deployed with Nginx.<br /><br />Skills: Node.js, Express.js, MongoDB, Nginx, React", "Sept 2017 - Aug 2018", "Waterloo", ""],
 
     [ "Ciena","", "Embedded Software Intern", "Implemented algorithms to analyze file structures and created many central web pages displaying analytics from multiple sources for our new team.<br /><br />Skills: Visual Basic, Angular, D3.js", "Summer 2016", "Ottawa-Kanata", ""],
 
