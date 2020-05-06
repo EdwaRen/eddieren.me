27c3fcbe8d8b6115113da6e46c863e934f0ebccf
Eddie <eddie.ren.2013@gmail.com>
2020-05-06 16:56:52 -0400

    move file

--- src/Components/MobileDesc.jsx
+++ src/Components/MobileDesc.jsx
@@ -96,7 +96,7 @@ function Desc(props) {
   // Creates the subtitle in description container which supports formatting. Has conditionning to trigger an alternate simplified 'basic' page.
   var subtitle = <h1></h1>
   if (props.subtitle == "alternate") {
-    subtitle = <p id = "fileSubTitle" style = {{fontSize: "14px", fontFamily: "RalewayRegIta",color: "#0000EE", marginTop: "-17px", textAlign: "center"}} ><a target="_blank"  href = "Edward_Ren_Resume.pdf">Resume</a></p>
+    subtitle = <p id = "fileSubTitle" style = {{fontSize: "14px", fontFamily: "RalewayRegIta",color: "#0000EE", marginTop: "-17px", textAlign: "center"}} ><a target="_blank"  href = "app/Edward_Ren_Resume.pdf">Resume</a></p>
   } else {
     subtitle = <p id="fileSubTitle" style = {{fontSize: "14px", fontFamily: "RalewayRegIta", marginTop: "-17px", textAlign: "center"}} dangerouslySetInnerHTML={{
       __html: props.subtitle
