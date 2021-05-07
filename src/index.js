590407875405122f461c8ecfcff95383036267fb
Eddie <eddie.ren.2013@gmail.com>
2021-05-07 14:35:52 -0400

    fix background

--- src/index.js
+++ src/index.js
@@ -3,26 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './Components/App';
 
-
-
-// class Background extends React.Component {
-//   constructor() {
-//     super();
-//   }
-//   render() {
-//
-//     return (
-//       <div id = "bodyBackground"></div>
-//
-//     )
-//   }
-
-
-
-// }
-// ========================================
-
-
 ReactDOM.render(
     <App />,
 
